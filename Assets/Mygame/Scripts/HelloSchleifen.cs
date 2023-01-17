b4c4463530e0ef168b2d90717a783e617eae8e7e
bgreis <benedikt.greisberger04@htl-salzburg.ac.at>
2023-01-17 20:07:10 +0100

    HelloSchleifen

---
+++ Assets/Mygame/Scripts/HelloSchleifen.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloSchleifen : MonoBehaviour
+{
+    public int[][] array = new int[5] [];
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
